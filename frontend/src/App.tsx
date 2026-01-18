50bf533472e51dd47a2115067cb5a30c4bac507c
Lucianogalizia <lucianogalizia170@hotmail.com>
2026-01-18 09:28:37 -0300

    Initialize React Router with main routes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import UploadTasks from "./pages/UploadTasks";
+import Tasks from "./pages/Tasks";
+import TaskDetail from "./pages/TaskDetail";
+import Dashboard from "./pages/Dashboard";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/upload" element={<UploadTasks/>} />
+        <Route path="/tasks" element={<Tasks/>} />
+        <Route path="/task/:taskId" element={<TaskDetail/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
