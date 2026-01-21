205a09bcfbcebc8ac1af646fd7fd2d7c6ce9fb57
Lucianogalizia <lucianogalizia170@hotmail.com>
2026-01-21 15:19:20 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,23 @@ import UploadTasks from "./pages/UploadTasks";
 import Tasks from "./pages/Tasks";
 import TaskDetail from "./pages/TaskDetail";
 import Dashboard from "./pages/Dashboard";
-import Uploads from "./pages/Uploads"; // ✅ NUEVO
+import TopNav from "./components/TopNav";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/upload" element={<UploadTasks />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/task/:taskId" element={<TaskDetail />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/uploads" element={<Uploads />} /> {/* ✅ NUEVO */}
-      </Routes>
+      <TopNav />
+
+      <div className="pt-2">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/upload" element={<UploadTasks />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/task/:taskId" element={<TaskDetail />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
+
